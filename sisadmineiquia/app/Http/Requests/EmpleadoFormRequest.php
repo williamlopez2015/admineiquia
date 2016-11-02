3b277a59484f8e6f68b4857b965e5542ee121230
WalterVelis <waltervelis45@gmail.com>
2016-11-01 20:20:16 -0600

    Merge remote-tracking branch 'refs/remotes/origin/bautista' into Walter
    # Conflicts:

--- sisadmineiquia/app/Http/Requests/EmpleadoFormRequest.php
+++ sisadmineiquia/app/Http/Requests/EmpleadoFormRequest.php
@@ -1,6 +1,9 @@
 <?php
 namespace sisadmineiquia\Http\Requests;
 use sisadmineiquia\Http\Requests\Request;
+
+
+
 class EmpleadoFormRequest extends Request
 {
     /**
@@ -29,7 +32,20 @@ class EmpleadoFormRequest extends Request
         'dui'=>'required|max:10',
         'nit'=>'required|max:20',
         'isss'=>'required|max:10',
-        'afp'=>'required|max:12'
+        'afp'=>'required|max:12',
+        'sexo'=>'required|in:F,M'
         ];
     }
+
 }
+
+
+    public function messages()
+    {
+    	return ['primernombre.required' =>'El campo Primer Nombre es obligatorio',
+    			'primerapellido.required' =>'El campo Primer Apellido es obligatorio',
+    			'segundopellido.required' =>'El campo Segundo Apellido es obligatorio',
+    			'sexo.in'=>'Seleccione un Sexo Valido'];
+ 	}
+}
+
