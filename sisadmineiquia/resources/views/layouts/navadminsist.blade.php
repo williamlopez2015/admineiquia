3b639dc9da0452e229e322c7ac1719c13c45ed56
williamlopez2015 <rogelwilliam@gmail.com>
2016-11-07 22:48:23 -0600

    Iconos
    Icnonos  del navadmin modificados

--- sisadmineiquia/resources/views/layouts/navadminsist.blade.php
+++ sisadmineiquia/resources/views/layouts/navadminsist.blade.php
@@ -2,57 +2,57 @@
         <a href="/home"><i class="fa fa-fw fa-home"></i> Inicio</a>
     </li>
     <li>
-        <a href="/admin/users"><i class="fa fa-fw fa-user"></i> Administrar Usuario</a>
+        <a href="/admin/users"><i class="glyphicon glyphicon-user"></i> Administrar Usuario</a>
     </li>
     <li >
-        <a href="#" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-paperclip"></i> <span>Gestion de Empleados</span> <i class="fa fa-angle-left pull-right"></i></a>
+        <a href="#" data-toggle="collapse" data-target="#demo"><i class="glyphicon glyphicon-th-list"></i> <span>Gestion de Empleados</span> <i class="fa fa-angle-left pull-right"></i></a>
 
         <ul id="demo" class="collapse">
 
            <li>
-                <a href="/admin/empleado/create"><i class="fa fa-plus"></i> Agregar Empleado</a>
+                <a href="/admin/empleado/create"><i class="glyphicon glyphicon-floppy-disk"></i> Agregar Empleado</a>
             </li>
             <li>
                 <a href="/admin/empleado"><i class="fa fa-users"></i> Administrar Empleados</a>
             </li>
             <li>
-                <a href="empleado/nominareport"><i class="fa fa-file"></i> Nomina Empleados</a>
+                <a href="empleado/nominareport"><i class="glyphicon glyphicon-print"></i> Nomina Empleados</a>
             </li>
         </ul>
     </li>
     <li >
-        <a href="#" data-toggle="collapse" data-target="#demo1"><i class="fa fa-fw fa-paperclip"></i> <span>Gestion Expediente Administrativo</span> <i class="fa fa-angle-left pull-right"></i></a>
+        <a href="#" data-toggle="collapse" data-target="#demo1"><i class="glyphicon glyphicon-folder-open"></i> <span>Gestion Expediente Administrativo</span> <i class="fa fa-angle-left pull-right"></i></a>
 
         <ul id="demo1" class="collapse">
 
            <li>
-                <a href="/admin/expedienteadministrativo/create"><i class="fa fa-plus"></i> Agregar Expediente </a>
+                <a href="/admin/expedienteadministrativo/create"><i class="glyphicon glyphicon-folder-close"></i> Agregar Expediente </a>
             </li>
             <li>
                 <a href="/admin/expedienteadministrativo"><i class="fa fa-users"></i> Administrar Expedientes</a>
             </li>
             <li>
-                <a href="/admin/tiempo/create"><i class="fa fa-plus"></i> Agregar Tiempo Adicional</a>
+                <a href="/admin/tiempo/create"><i class="glyphicon glyphicon-time"></i> Agregar Tiempo Adicional</a>
             </li>
         </ul>
     </li>
     <li>
-        <a href="#" data-toggle="collapse" data-target="#demo2"><i class="fa fa-fw fa-paperclip"></i> <span>Gestionar Expediente Academico</span> <i class="fa fa-angle-left pull-right"></i></a>
+        <a href="#" data-toggle="collapse" data-target="#demo2"><i class="glyphicon glyphicon-education"></i> <span>Gestionar Expediente Academico</span> <i class="fa fa-angle-left pull-right"></i></a>
         <ul id="demo2" class="collapse">
             <li>
-                <a href="/admin/expedienteacademico/create"><i class="fa fa-plus"></i> Agregar Expediente</a>
+                <a href="/admin/expedienteacademico/create"><i class="glyphicon glyphicon-floppy-saved"></i> Agregar Expediente</a>
             </li>
             <li>
                 <a href="/admin/expedienteacademico"><i class="fa fa-users"></i> Administrar Expediente</a>
             </li>
             <li>
-                <a href="/admin/cargaacademica/create"><i class="fa fa-plus"></i> Agregar Carga</a>
+                <a href="/admin/cargaacademica/create"><i class="glyphicon glyphicon-book"></i> Agregar Carga</a>
             </li>
             <li>
-                <a href="/admin/cargaacademica"><i class="fa fa-users"></i> Gestionar Carga</a>
+                <a href="/admin/cargaacademica"><i class="glyphicon glyphicon-pencil"></i> Gestionar Carga</a>
             </li>
             <li>
-                <a href="/admin/experiencialaboralacademica/create"><i class="fa fa-plus"></i> Agregar Experiencia Laboral Academica</a>
+                <a href="/admin/experiencialaboralacademica/create"><i class="glyphicon glyphicon-tasks"></i> Agregar Experiencia Laboral Academica</a>
             </li>
             <li>
                 <a href="/admin/experiencialaboralacademica"><i class="fa fa-users"></i> Gestionar Experiencia Laboral Academica</a>
@@ -60,36 +60,36 @@
         </ul>
     </li>
     <li>
-        <a href="#" data-toggle="collapse" data-target="#demo3"><i class="fa fa-fw fa-paperclip"></i> Gestionar Puestos <i class="fa fa-angle-left pull-right"></i></a>
+        <a href="#" data-toggle="collapse" data-target="#demo3"><i class="glyphicon glyphicon-equalizer"></i> Gestionar Puestos <i class="fa fa-angle-left pull-right"></i></a>
         <ul id="demo3" class="collapse">
             <li>
-                <a href="/admin/puesto/create"><i class="fa fa-plus"></i> Agregar Puesto</a>
+                <a href="/admin/puesto/create"><i class="glyphicon glyphicon-pushpin"></i> Agregar Puesto</a>
             </li>
             <li>
                 <a href="/admin/perfilpuesto"><i class="fa fa-users"></i> Administrar Perfiles</a>
             </li>
             <li>
-                <a href="/admin/puesto"><i class="fa fa-users"></i> Administrar Puestos</a>
+                <a href="/admin/puesto"><i class="glyphicon glyphicon-object-align-right"></i> Administrar Puestos</a>
             </li>
             
         </ul>
     </li>
     <li>
-        <a href="javascript:;" data-toggle="collapse" data-target="#demo4"><i class="fa fa-fw fa-paperclip"></i> Control de Asistencia<i class="fa fa-angle-left pull-right"></i></a>
+        <a href="javascript:;" data-toggle="collapse" data-target="#demo4"><i class="glyphicon glyphicon-ok"></i> Control de Asistencia<i class="fa fa-angle-left pull-right"></i></a>
         <ul id="demo4" class="collapse">
             <li>
-                <a href="/admin/asistencia/create"><i class="fa fa-plus"></i> Agregar Asistencia</a>
+                <a href="/admin/asistencia/create"><i class="glyphicon glyphicon-open-file"></i> Agregar Asistencia</a>
             </li>
             <li>
                 <a href="/admin/asistencia"><i class="fa fa-users"></i> Gestionar Asistencia</a>
             </li>
             <li>
-                <a href="#"><i class="fa fa-file"></i> Reporte de asistencia</a>
+                <a href="#"><i class="glyphicon glyphicon-sort-by-alphabet"></i> Reporte de asistencia</a>
             </li>
         </ul>
     </li>
     <li>
-        <a href="#" data-toggle="collapse" data-target="#demo5"><i class="fa fa-fw fa-paperclip"></i> Gestionar Permisos Expediente <i class="fa fa-angle-left pull-right"fa fa-angle-left pull-right></i></a>
+        <a href="#" data-toggle="collapse" data-target="#demo5"><i class="glyphicon glyphicon-list-alt"></i> Gestionar Permisos Expediente <i class="fa fa-angle-left pull-right"fa fa-angle-left pull-right></i></a>
         <ul id="demo5" class="collapse">
             <li>
                 <a href="/admin/permiso/create"><i class="fa fa-plus"></i> Agregar Permiso</a>
@@ -100,13 +100,13 @@
         </ul>
     </li>
     <li>
-        <a href="#" data-toggle="collapse" data-target="#demo6"><i class="fa fa-fw fa-paperclip"></i> Gestionar de Acuerdo<i class="fa fa-angle-left pull-right"></i></a>
+        <a href="#" data-toggle="collapse" data-target="#demo6"><i class="glyphicon glyphicon-transfer"></i> Gestionar de Acuerdo<i class="fa fa-angle-left pull-right"></i></a>
         <ul id="demo6" class="collapse">
             <li>
-                <a href="/admin/acuerdos/create"><i class="fa fa-plus"></i> Agregar Acuerdo</a>
+                <a href="/admin/acuerdos/create"><i class="glyphicon glyphicon-floppy-open"></i> Agregar Acuerdo</a>
             </li>
             <li>
-                <a href="/admin/acuerdos"><i class="fa fa-users"></i> Administar Acuerdos</a>
+                <a href="/admin/acuerdos"><i class="glyphicon glyphicon-saved"></i> Administar Acuerdos</a>
             </li>
         </ul>
     </li>
