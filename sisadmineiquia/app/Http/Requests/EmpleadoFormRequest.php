effeaff76865d49cbafa9699d2a8cb059b599e87
kratos <bautista.insu@gmail.com>
2016-12-07 19:47:33 -0600

    validado empleado

--- sisadmineiquia/app/Http/Requests/EmpleadoFormRequest.php
+++ sisadmineiquia/app/Http/Requests/EmpleadoFormRequest.php
@@ -28,8 +28,8 @@ class EmpleadoFormRequest extends Request
         'primerapellido'=>'required|max:50',
         'segundoapellido'=>'required|max:50',
         'primerapellido'=>'required|max:50',
-        'dui'=>'required|max:10',
-        'nit'=>'required|max:20',
+        'dui'=>'required|max:10|regex:/^[0-9]{8}[-]{1}[0-9]{1}$/i',
+        'nit'=>'required|max:20|regex:/^[0-9]{4}[-]{1}[0-9]{6}[-]{1}[0-9]{3}[-]{1}[0-9]{1}$/i',
         'isss'=>'required|max:10',
         'afp'=>'required|max:12',
         'sexo'=>'required|in:F,M',
@@ -43,7 +43,13 @@ class EmpleadoFormRequest extends Request
     	return ['primernombre.required' =>'El campo Primer Nombre es obligatorio',
     			'primerapellido.required' =>'El campo Primer Apellido es obligatorio',
     			'segundopellido.required' =>'El campo Segundo Apellido es obligatorio',
-    			'sexo.in'=>'Seleccione un Sexo Valido'];
+                'dui.required' =>'El campo Documento de Identidad es obligatorio',
+                'nit.required' =>'El campo Numero de Identificaion Tributaria es obligatorio',
+                'isss.required' =>'El campo Numero de Seguro Social es obligatorio',
+                'afp.required' =>'El campo Numero de AFP es obligatorio',
+    			'sexo.in'=>'Seleccione un Sexo Valido',
+                'dui.regex' =>'El campo Documento de Identidad no cumple el formato 00000000-0',
+                 'nit.regex' =>'El campo Numero de Identificacion Tributaria no cumple el formato 0000-000000-000-0'];
  	}
 
 }
